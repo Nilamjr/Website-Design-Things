e5995d797be6bedbcbf312d64576b745ec407c32
Nilamjr <nilamrj07@gmail.com>
2022-08-20 18:45:21 +0530

    Practicing navbar

---
+++ NavbarWithKevin/practiceNavbar/app.js
@@ -0,0 +1,7 @@
+const primaryNavigation = document.querySelector('.primary-navigation');
+const mobileToggle = document.querySelector('.mobile-nav-toggle');
+
+
+mobileToggle.addEventListener("click", ()=>{
+    console.log("click");
+});
