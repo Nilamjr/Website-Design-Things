0c99a63a89a526304d8989024ba5a4256065c242
Nilamjr <nilamrj07@gmail.com>
2022-08-21 17:11:08 +0530

    Navbar Practivce done and Accordion started

--- NavbarWithKevin/practiceNavbar/app.js
+++ NavbarWithKevin/practiceNavbar/app.js
@@ -3,5 +3,19 @@ const mobileToggle = document.querySelector('.mobile-nav-toggle');
 
 
 mobileToggle.addEventListener("click", ()=>{
-    console.log("click");
+
+    const dataVisible = primaryNavigation.getAttribute('data-visible');
+    // const areaExpand = mobileToggle.getAttribute('aria-expanded');
+
+    // console.log("Get Attribute", dataVisible);
+    // console.log("Get Attribute2", areaExpand);
+
+    if(dataVisible == "false"){
+        primaryNavigation.setAttribute("data-visible", true);
+        mobileToggle.setAttribute("aria-expanded", true);
+    }else{
+        primaryNavigation.setAttribute("data-visible", false);
+        mobileToggle.setAttribute("aria-expanded", false);
+    }
+
 });
